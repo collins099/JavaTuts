39fe86ed7360953fc80c3dcfc1badb3a05961b1d
Collins Acquah <collins09acquah@gmail.com>
2022-04-29 11:25:11 +0000

    added another line

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ package com.carona;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO Emma");
+        System.out.println("How are you Emma");
 
     }
 }
